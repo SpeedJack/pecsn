e44361a1765c675f8951ac37b26617a4cff88015
Niccolò Scatena <speedjack95@gmail.com>
2021-01-03 10:03:14 +0100

    Remove license comments

--- src/oracle.cc
+++ src/oracle.cc
@@ -1,18 +1,3 @@
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
-
 #include "oracle.h"
 
 namespace pecsn {
@@ -34,7 +19,7 @@ void Oracle::initialize()
 
     quantileTime = registerSignal("quantileTime");
 
-    scheduleAt(simTime(), slotMessage); // start slotting now
+    scheduleAt(simTime(), slotMessage); // slotting
 }
 
 void Oracle::handleMessage(cMessage *msg)
@@ -43,8 +28,8 @@ void Oracle::handleMessage(cMessage *msg)
            return;
 
     if(timer == 0)
-        endSimulation(); //if no events arrived before the timer the simulation ends
-    timer--; //no events arrived
+        endSimulation();
+    timer--;
     scheduleAt(simTime() + slotDuration, slotMessage);
 }
 
