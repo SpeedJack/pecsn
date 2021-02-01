0bba08b8000c73171850f7f5e9d7c075625b0c76
Niccolò Scatena <speedjack95@gmail.com>
2021-02-01 18:29:47 +0100

    Add 2kr assumptions tests jupyter notebook

---
+++ simulations/simulate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+VECTORS=false
+REPEAT=10
+while getopts s:c:o:r:v flag
+do
+	case "${flag}" in
+		s) SCENARIO=${OPTARG};;
+		c) CONFIGNAME=${OPTARG};;
+		o) OUTNAME=${OPTARG};;
+		r) REPEAT=${OPTARG};;
+		v) VECTORS=true;;
+	esac
+done
+
+if [ -z "$SCENARIO" ] || [ -z "$CONFIGNAME" ]; then
+	echo '-c and -s parameters are required.'
+	exit
+fi
+if [ -z "$OUTBASENAME" ]; then
+	OUTBASENAME=$CONFIGNAME
+fi
+
+rm -rf results/$CONFIGNAME
+./run -c $CONFIGNAME -f simulations.ini -u Cmdenv --repeat=$REPEAT
+mkdir -p ../analysis/$SCENARIO/exported_data
+if [ "$VECTORS" = true ]; then
+	scavetool x results/$CONFIGNAME/*.{sca,vec} -o ../analysis/$SCENARIO/exported_data/$OUTNAME.csv
+else
+	scavetool x results/$CONFIGNAME/*.sca -o ../analysis/$SCENARIO/exported_data/$OUTNAME.csv
+fi
